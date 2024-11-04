8a5b069ab4f968688a687955fdda36298b6f39d8
Ziv Lee <minjer@foxmail.com>
2024-11-04 17:47:04 +0800

    更新模板

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'Minjer',
   subtitle: 'Blog Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -17,7 +17,16 @@ export const siteConfig: SiteConfig = {
   banner: {
     enable: true,
     src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'top', // Equivalent to object-position, defaults center
+    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    credit: {
+      enable: false,         // Display the credit text of the banner image
+      text: '',              // Credit text to be displayed
+      url: ''                // (Optional) URL link to the original artwork or artist's page
+    }
+  },
+  toc: {
+    enable: true,           // Display the table of contents on the right side of the post
+    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
     // {
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,4 @@
-import type { LIGHT_MODE, DARK_MODE, AUTO_MODE } from "@constants/constants"
+import type { AUTO_MODE, DARK_MODE, LIGHT_MODE } from '@constants/constants'
 
 export type SiteConfig = {
   title: string
@@ -13,7 +13,16 @@ export type SiteConfig = {
   banner: {
     enable: boolean
     src: string
-    position?: string
+    position?: 'top' | 'center' | 'bottom'
+    credit: {
+      enable: boolean
+      text: string
+      url?: string
+    }
+  }
+  toc: {
+    enable: boolean
+    depth: 1 | 2 | 3
   }
 
   favicon: Favicon[]
@@ -58,4 +67,22 @@ export type LicenseConfig = {
   url: string
 }
 
-export type LIGHT_DARK_MODE = typeof LIGHT_MODE | typeof DARK_MODE | typeof AUTO_MODE
+export type LIGHT_DARK_MODE =
+  | typeof LIGHT_MODE
+  | typeof DARK_MODE
+  | typeof AUTO_MODE
+
+export type BlogPostData = {
+  body: string
+  title: string
+  published: Date
+  description: string
+  tags: string[]
+  draft?: boolean
+  image?: string
+  category?: string
+  prevTitle?: string
+  prevSlug?: string
+  nextTitle?: string
+  nextSlug?: string
+}
